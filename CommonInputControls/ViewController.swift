0334d3a037ec02c0abc2eb21f02733cb3f8d1072
Sim Yong Seng <simyseng@gmail.com>
2019-02-11 20:55:40 +0700

    Final code

--- CommonInputControls/ViewController.swift
+++ CommonInputControls/ViewController.swift
@@ -10,11 +10,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    @IBAction func buttonTapped(_ sender: Any) {
+        print("Button was tapped")
+    }
 
-
+    @IBAction func SwitchTapped(_ sender:UISwitch) {
+        if sender.isOn {
+            print("Turn on.")
+        }
+        else {
+            print("Turn off")
+        }
+    }
 }
 
